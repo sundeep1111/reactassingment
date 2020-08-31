e33cd66466e69d448e20a6ff073a1040d78de535
sundeep <sundeep1111-code@gmail.com>
2020-09-01 00:02:39 +0530

    Updates

---
+++ precache-manifest.53a86ebf15d3a8c923f2afdb70997cd7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbae0783da0b1f78e19362155da9403",
+    "url": "/reactassingment/index.html"
+  },
+  {
+    "revision": "5cef1061fba2762ee8d9",
+    "url": "/reactassingment/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cde27eb9a7f3d7bfcf54",
+    "url": "/reactassingment/static/css/main.a3b57f09.chunk.css"
+  },
+  {
+    "revision": "5cef1061fba2762ee8d9",
+    "url": "/reactassingment/static/js/2.24d0b59c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactassingment/static/js/2.24d0b59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde27eb9a7f3d7bfcf54",
+    "url": "/reactassingment/static/js/main.c38f1d30.chunk.js"
+  },
+  {
+    "revision": "beab1882a6fc6fa68d5f",
+    "url": "/reactassingment/static/js/runtime-main.8dea30fa.js"
+  },
+  {
+    "revision": "adfc2ab40e78eb51b47729785de19c1f",
+    "url": "/reactassingment/static/media/arrow-down.adfc2ab4.png"
+  },
+  {
+    "revision": "881e4ec8e9e4ce91cacc8cf8475eb789",
+    "url": "/reactassingment/static/media/arrow-up.881e4ec8.png"
+  },
+  {
+    "revision": "5044cc0bc6e36b600c32c1772ae813b7",
+    "url": "/reactassingment/static/media/landing-section-image.5044cc0b.png"
+  },
+  {
+    "revision": "d43e2fd40a220c8a0aff6eb9181cc2a3",
+    "url": "/reactassingment/static/media/stunning-logo.d43e2fd4.png"
+  },
+  {
+    "revision": "330ba7b090d196001b486bfb9ed8a19e",
+    "url": "/reactassingment/static/media/tick-blue.330ba7b0.png"
+  },
+  {
+    "revision": "289635ccdbc5a4b5f1468d8459e632b8",
+    "url": "/reactassingment/static/media/tick-gray.289635cc.png"
+  },
+  {
+    "revision": "6bf196c58f593de5fc5e5bf4eaca5873",
+    "url": "/reactassingment/static/media/user-blue.6bf196c5.png"
+  },
+  {
+    "revision": "29fbde9dc87aab174b40152545b6e1fe",
+    "url": "/reactassingment/static/media/user-orange.29fbde9d.png"
+  },
+  {
+    "revision": "9f9b7199e6173f137432ece1c8bf2522",
+    "url": "/reactassingment/static/media/user-pink.9f9b7199.png"
+  },
+  {
+    "revision": "aeb86ae0f05c13486c9786f58befa393",
+    "url": "/reactassingment/static/media/user-red.aeb86ae0.png"
+  }
+]);
